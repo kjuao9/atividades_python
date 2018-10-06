df878e8788353ffdc59b5a44d4e156665f932727
João Paulo S. Costa <43136205+kjuao9@users.noreply.github.com>
2018-10-06 17:49:15 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,3 +1,25 @@
 import math
-def fator(n):
-    
+def fator(a,b):
+    while True:
+        print("""
+             1-adição
+             2-subtração
+             3-divisão
+             4-multiplicação
+             5-sair             
+             """)
+        op = 0
+        op = int(input("Digite uma opção: "))
+        if op == 1:
+            return a + b
+        if op == 2:
+            return a - b
+        if op == 3:
+            return a / b
+        if op == 4:
+            return a * b
+        if op == 5:
+            break
+            
+            
+              
